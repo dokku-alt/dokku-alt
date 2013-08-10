1f146435f561b6d0cd091c6555b24c7a5d0b4e3e
Kload <kload@kload.fr>
2013-08-10 14:29:55 +0000

    bugfix

--- start_pgsql.sh
+++ start_pgsql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # forked from https://gist.github.com/jpetazzo/5494158
 
+echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.1/main/pg_hba.conf
 su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres           -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf  -c listen_addresses=*" 
