6cfab7ed9bd4db9b14ead4c222a535ed78ee6826
Kload <kload@kload.fr>
2013-08-11 14:44:06 +0200

    bugfix

--- prepare_mariadb.sh
+++ prepare_mariadb.sh
@@ -2,9 +2,9 @@
 
 mysqld_safe & sleep 5
 echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
-echo "UPDATE user set host=’%’ where user=’root’;" | mysql -u root --password=a_stronk_password
+echo "UPDATE user set host=’%’ where user=’root’;" | mysql -u root --password=a_stronk_password -p 'mysql'
 if [[ ! -z $2 ]]; then
     echo "$2" | mysql -u root --password=a_stronk_password -p 'db'
 fi
-echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password
+echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password -p 'mysql'
 exit 0
