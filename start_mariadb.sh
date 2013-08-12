fdb17ee7f0a4bd6b469cef92bb587276fa84261e
Kload <kload@kload.fr>
2013-08-12 19:00:00 +0200

    Remove useless SQL import at startup

--- start_mariadb.sh
+++ start_mariadb.sh
@@ -6,7 +6,5 @@ chown -R mysql:mysql /opt/mysql
 chmod -R 755 /opt/mysql
 mysqld_safe & sleep 5
 echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
-echo "$2" | mysql -u root --password=a_stronk_password db
 echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password mysql
 echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '$1'; FLUSH PRIVILEGES;" | mysql -u root --password="$1" mysql
-#tailf /var/log/mysql.log
