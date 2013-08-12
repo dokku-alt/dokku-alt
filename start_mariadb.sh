db3d2d663d57567a1852e255631f520a66b4f015
Kload <kload@kload.fr>
2013-08-12 11:43:54 +0200

    Bugfixes

--- start_mariadb.sh
+++ start_mariadb.sh
@@ -4,7 +4,7 @@ mkdir -p /opt/mysql
 cp -a /var/lib/mysql/* /opt/mysql/
 chown -R mysql:mysql /opt/mysql
 chmod -R 755 /opt/mysql
-mysqld
+mysqld & sleep 5
 echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
 echo "$2" | mysql -u root --password=a_stronk_password db
 echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password mysql
