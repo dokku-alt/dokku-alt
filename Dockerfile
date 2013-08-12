1479b873140c11c6b55f4c8d777440daf60d361c
Kload <kload@kload.fr>
2013-08-12 19:44:58 +0200

    User persistent storage

--- Dockerfile
+++ Dockerfile
@@ -16,3 +16,5 @@ RUN	rm /usr/sbin/policy-rc.d
 
 ADD	. /usr/bin
 RUN	chmod +x /usr/bin/start_pgsql.sh
+RUN echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.1/main/pg_hba.conf
+RUN sed -i -e"s/var\/lib/opt/g" /etc/postgresql/9.1/main/postgresql.conf
--- start_pgsql.sh
+++ start_pgsql.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # forked from https://gist.github.com/jpetazzo/5494158
 
-echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.1/main/pg_hba.conf
+mkdir -p /opt/postgresql
+cp -a /var/lib/postgresql/* /opt/postgresql/
+chown -R postgres:postgres /opt/postgresql
 su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
 su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE DATABASE db;"
 su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres           -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf  -c listen_addresses=*"
