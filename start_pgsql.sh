be3af0846e1502b6161931ebcaab23371882306f
Kload <kload@kload.fr>
2013-08-13 10:58:46 +0200

    Give the ability to start without changing the root password

--- start_pgsql.sh
+++ start_pgsql.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# forked from https://gist.github.com/jpetazzo/5494158
 
-mkdir -p /opt/postgresql
-cp -a /var/lib/postgresql/* /opt/postgresql/
-chown -R postgres:postgres /opt/postgresql
-su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
-su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE DATABASE db;"
+if [[ ! -z "$1" ]]; then
+    mkdir -p /opt/postgresql
+    cp -a /var/lib/postgresql/* /opt/postgresql/
+    chown -R postgres:postgres /opt/postgresql
+    su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
+    su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE DATABASE db;"
+fi
 su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres           -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf  -c listen_addresses=*"
