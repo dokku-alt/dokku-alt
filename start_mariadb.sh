80cd56995fd09deecb139b63e53692dedd529385
Kload <kload@kload.fr>
2013-08-13 11:06:05 +0200

    Give the ability to start without changing the root password

--- start_mariadb.sh
+++ start_mariadb.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-mkdir -p /opt/mysql
-cp -a /var/lib/mysql/* /opt/mysql/
-chown -R mysql:mysql /opt/mysql
-chmod -R 755 /opt/mysql
+if [[ ! -z "$1" ]]; then
+    mkdir -p /opt/mysql
+    cp -a /var/lib/mysql/* /opt/mysql/
+    chown -R mysql:mysql /opt/mysql
+    chmod -R 755 /opt/mysql
+fi
 mysqld_safe & sleep 5
-echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
-echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password mysql
-echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '$1'; FLUSH PRIVILEGES;" | mysql -u root --password="$1" mysql
+
+if [[ ! -z "$1" ]]; then
+    echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
+    echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password mysql
+    echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '$1'; FLUSH PRIVILEGES;" | mysql -u root --password="$1" mysql
+fi
