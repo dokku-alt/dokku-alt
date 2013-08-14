e00b69ea13e8ce0133b0bfbd677da354a78d7215
Kload <kload@kload.fr>
2013-08-14 12:28:07 +0200

    Reattach job to keep the process up

--- start_mariadb.sh
+++ start_mariadb.sh
@@ -7,9 +7,9 @@ if [[ ! -z "$1" ]]; then
     chmod -R 755 /opt/mysql
 fi
 mysqld_safe & sleep 5
-
 if [[ ! -z "$1" ]]; then
     echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
     echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password mysql
     echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '$1'; FLUSH PRIVILEGES;" | mysql -u root --password="$1" mysql
 fi
+fg
