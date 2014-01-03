d716d2978c045c8a036ba8557b9f542d11f1c985
Alexis Gavoty <kload@kload.fr>
2014-01-03 16:10:17 +0100

    Update start_mariadb.sh

--- start_mariadb.sh
+++ start_mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -z "$1" ]]; then
+if [[ ! -f /opt/mysql/initialized ]]; then
     mkdir -p /opt/mysql
     cp -a /var/lib/mysql/* /opt/mysql/
     chown -R mysql:mysql /opt/mysql
@@ -9,9 +9,10 @@ fi
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 sed -i -e"s/var\/lib/opt/g" /etc/mysql/my.cnf
 mysqld_safe & sleep 5
-if [[ ! -z "$1" ]]; then
+if [[ ! -f /opt/mysql/initialized ]]; then
     echo "CREATE DATABASE db;" | mysql -u root --password=a_stronk_password
     echo "UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root --password=a_stronk_password mysql
     echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '$1'; FLUSH PRIVILEGES;" | mysql -u root --password="$1" mysql
+    touch /opt/mysql/initialized
 fi
 tailf /var/log/mysql.log
