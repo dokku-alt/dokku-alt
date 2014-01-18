fb67c79f6c3b7c0fbc77271e9424f64e221f5dfb
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2014-01-19 00:30:04 +0100

    running serer in background for user change

--- start-rabbitmq.sh
+++ start-rabbitmq.sh
@@ -4,19 +4,16 @@ if [[ ! -f /opt/rabbitmq/initialized ]]; then
     mkdir -p /opt/rabbitmq
     cp -a /var/lib/rabbitmq/mnesia/* /opt/rabbitmq/
     chown -R rabbitmq:rabbitmq /opt/rabbitmq
-
-    export RABBITMQ_MNESIA_BASE=/opt/rabbitmq
-    /usr/sbin/rabbitmq-server &
-    # add new user
-    rabbitmqctl add_user admin $1
-    rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
-    rabbitmqctl set_user_tags admin administrator
-    # remove default user
-    rabbitmqctl delete_user guest
-    rabbitmqctl stop
     touch /opt/rabbitmq/initialized
-    /usr/sbin/rabbitmq-server
-else
-    export RABBITMQ_MNESIA_BASE=/opt/rabbitmq
-    /usr/sbin/rabbitmq-server
 fi
+
+export RABBITMQ_MNESIA_BASE=/opt/rabbitmq
+/usr/sbin/rabbitmq-server -detached
+# add new user
+rabbitmqctl add_user admin $1
+rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+rabbitmqctl set_user_tags admin administrator
+# remove default user
+rabbitmqctl delete_user guest
+rabbitmqctl stop
+/usr/sbin/rabbitmq-server
