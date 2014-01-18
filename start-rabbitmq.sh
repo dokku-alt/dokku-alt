659f1e553a0061ede4867ca28257ca5899ab0610
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2014-01-18 22:06:03 +0100

    administrator tag added

--- start-rabbitmq.sh
+++ start-rabbitmq.sh
@@ -4,11 +4,13 @@ if [[ ! -f /opt/rabbitmq/initialized ]]; then
     mkdir -p /opt/rabbitmq
     cp -a /var/lib/rabbitmq/mnesia/* /opt/rabbitmq/
     chown -R rabbitmq:rabbitmq /opt/rabbitmq
+
     export RABBITMQ_MNESIA_BASE=/opt/rabbitmq
     /usr/sbin/rabbitmq-server
     # add new user
     rabbitmqctl add_user admin $1
     rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+    rabbitmqctl set_user_tags admin administrator
     # remove default user
     rabbitmqctl delete_user guest
     touch /opt/rabbitmq/initialized
