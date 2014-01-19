b4559564af9dc54193fd6a7552f811da3dd22666
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2014-01-19 01:50:30 +0100

    admin user created only on init

--- start-rabbitmq.sh
+++ start-rabbitmq.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+export RABBITMQ_MNESIA_BASE=/opt/rabbitmq
+
 if [[ ! -f /opt/rabbitmq/initialized ]]; then
     mkdir -p /opt/rabbitmq
     cp -a /var/lib/rabbitmq/mnesia/* /opt/rabbitmq/
     chown -R rabbitmq:rabbitmq /opt/rabbitmq
     touch /opt/rabbitmq/initialized
+    /usr/sbin/rabbitmq-server -detached
+    sleep 6
+    # add new user
+    rabbitmqctl add_user admin $1
+    rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+    rabbitmqctl set_user_tags admin administrator
+    # remove default user
+    rabbitmqctl delete_user guest
+    rabbitmqctl stop
+    sleep 4
 fi
 
-export RABBITMQ_MNESIA_BASE=/opt/rabbitmq
-/usr/sbin/rabbitmq-server -detached
-sleep 6
-# add new user
-rabbitmqctl add_user admin $1
-rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
-rabbitmqctl set_user_tags admin administrator
-# remove default user
-rabbitmqctl delete_user guest
-rabbitmqctl stop
-sleep 4
 /usr/sbin/rabbitmq-server
