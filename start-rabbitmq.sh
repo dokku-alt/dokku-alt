ad030e870846ab0fcf66e3bca4b14b7c1068764e
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2014-01-24 17:20:15 +0100

    reset admin pass whenever supplied

--- start-rabbitmq.sh
+++ start-rabbitmq.sh
@@ -7,6 +7,8 @@ if [[ ! -f /opt/rabbitmq/initialized ]]; then
     cp -a /var/lib/rabbitmq/mnesia/* /opt/rabbitmq/
     chown -R rabbitmq:rabbitmq /opt/rabbitmq
     touch /opt/rabbitmq/initialized
+fi
+if [[ ! -z "$1" ]]; then
     /usr/sbin/rabbitmq-server -detached
     sleep 6
     # add new user
