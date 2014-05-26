4bfbc928ad1225acb995c3ce0e13ce3c0a386246
Kamil Trzciński <ayufan@ayufan.eu>
2014-05-26 11:47:35 +0200

    Fixed sshcommand and added bootstrap2 script

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -1,4 +1,15 @@
-curl -s https://get.docker.io/ubuntu/ | sudo sh
-wget http://dokku-alt.github.io/dokku-alt/dokku-alt.deb
-dpkg -i dokku-alt.deb
-apt-get -f install
+#!/bin/bash
+
+set -xe
+
+if [ ! -e /usr/lib/apt/methods/https ]; then
+	apt-get update
+	apt-get install -y apt-transport-https
+fi
+
+echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+echo deb http://dokku-alt.github.io/dokku-alt dokku-alt main > /etc/apt/sources.list.d/docker.list
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+apt-get update
+apt-get install dokku-alt
