d971cb9c7b988cef28f87ba70cfb67588347d733
Kamil Trzciński <ayufan@ayufan.eu>
2014-05-27 20:15:15 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,21 +8,23 @@ if [ ! -e /usr/lib/apt/methods/https ]; then
 fi
 
 echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
-echo deb http://dokku-alt.github.io/dokku-alt / > /etc/apt/sources.list.d/dokku-alt.list
+echo deb https://dokku-alt.github.io/dokku-alt / > /etc/apt/sources.list.d/dokku-alt.list
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 apt-key adv --keyserver keys.gnupg.net --recv-keys EAD883AF
 apt-get update
 apt-get install -y dokku-alt ruby ruby-sinatra
 
-if [[ `lsb_release -sr` != "14.04" ]]; then
+set +xe
+
+if [ `lsb_release -sr` != "14.04" ]; then
 	echo
 	echo "WARNING: dokku-alt works best on Ubuntu 14.04 LTS!"
 fi
 
 echo
 echo "Almost done!"
-echo "Open now web browser pointing to http://$(hostname):8080/ to finish configuartion."
+echo "Open now web browser pointing to http://$(hostname):2000/ to finish configuartion."
 echo "For manual installation instructions press Ctrl-C and visit https://github.com/dokku-alt/dokku-alt."
 echo
 
